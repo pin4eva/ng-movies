afd4bc31c312e7e825556d17232807807468006c
Peter Akaliro <pin4eva@hotmail.com>
2023-04-24 16:16:29 +0100

    first commit

---
+++ src/app/components/slider/slider.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'slider',
+  templateUrl: './slider.component.html',
+  styleUrls: ['./slider.component.scss']
+})
+export class SliderComponent {
+  @Input() items: any[] = [];
+}
---
+++ src/app/services/movies.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MoviesService {
+  constructor(private http: HttpClient) {}
+
+  getMovies() {
+    return this.http.get(
+      'https://api.themoviedb.org/3/movie/upcoming?api_key=905ac42be549e90bf96c0dca721a3cf0&language=en-US&page=1'
+    );
+  }
+}
